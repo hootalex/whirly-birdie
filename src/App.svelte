f76ddacdb430de39a93b218fb6fe22104eb4b714
Glitch (whirly-birdie) <none>
2020-03-01 18:55:38 +0000

    🎱👛 Updated with Glitch

---
+++ src/App.svelte
@@ -0,0 +1,35 @@
+<script>
+	export let name="alex";
+  import Nav from './Navigation.svelte'
+</script>
+
+
+<main>
+  <Nav/>
+	<h1>Hello {name}, welcome to Svelte!</h1>
+	<p><a href="https://glitch.com/edit/#!/svelte">Remix this on Glitch</a> and visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  
+  <div class="glitchButton" style="position:absolute;top:20px;right:20px;"></div>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
