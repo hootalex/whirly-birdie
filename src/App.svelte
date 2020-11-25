6650b50c8fedd7784cf0f4ba89056378d65165ac
Glitch (whirly-birdie) <none>
2020-11-25 15:30:19 +0000

    🍏🦂 Updated with Glitch

--- src/App.svelte
+++ src/App.svelte
@@ -127,7 +127,7 @@
 <style>
   @font-face {
     font-family: "Whirly Birdie";
-    src: url("https://cdn.glitch.com/1711052e-ea5e-4036-9c61-dd3ef6782855%2FWhirlyBirdieGX.ttf?v=1606275464452")
+    src: url("https://cdn.glitch.com/1711052e-ea5e-4036-9c61-dd3ef6782855%2FWhirlyBirdieVariable.ttf?v=1606318112621")
       format("truetype");
   }
 
