529d91ca614d4c01156c77512b62ff12f3ca8d7f
Glitch (whirly-birdie) <none>
2020-12-10 19:43:11 +0000

    <3!

--- src/App.svelte
+++ src/App.svelte
@@ -133,7 +133,7 @@
 
   @font-face {
     font-family: "Whirlybats";
-    src: url("https://cdn.glitch.com/1711052e-ea5e-4036-9c61-dd3ef6782855%2FWhirlybatsGX.ttf?v=1607478530697")
+    src: url("https://cdn.glitch.com/1711052e-ea5e-4036-9c61-dd3ef6782855%2FWhirlybatsGX.ttf?v=1607629059414")
       format("truetype");
   }
 
